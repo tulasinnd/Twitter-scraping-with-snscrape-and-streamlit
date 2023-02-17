ece7755b1aa8e34890c311898b2b908d29e63867
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-02-17 18:34:02 +0530

    Update Twitter_Scraper.py

--- Twitter_Scraper.py
+++ Twitter_Scraper.py
@@ -2,17 +2,11 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import streamlit as st
 import datetime
-import pymongo
-import time
- 
-# REQUIRED VARIABLES
-client = pymongo.MongoClient("mongodb://localhost:27017/")  # To connect to MONGODB
-mydb = client["Twitter_Database"]    # To create a DATABASE
+
 tweets_df = pd.DataFrame()
-dfm = pd.DataFrame()
 st.write("# Twitter data scraping")
 option = st.selectbox('How would you like the data to be searched?',('Keyword', 'Hashtag'))
-word = st.text_input('Please enter a '+option, 'Example: LIC Policy')
+word = st.text_input('Please enter a '+option, 'LIC Policy')
 start = st.date_input("Select the start date", datetime.date(2022, 1, 1),key='d1')
 end = st.date_input("Select the end date", datetime.date(2023, 1, 1),key='d2')
 tweet_c = st.slider('How many tweets to scrape', 0, 1000, 5)
@@ -22,7 +16,7 @@ tweets_list = []
 if word:
     if option=='Keyword':
         for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} + since:{start} until:{end}').get_items()):
-            if i>tweet_c:
+            if i>tweet_c-1:
                 break
             tweets_list.append([ tweet.id, tweet.date,  tweet.content, tweet.lang, tweet.user.username, tweet.replyCount, tweet.retweetCount,tweet.likeCount, tweet.source, tweet.url ])
         tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username', 'ReplyCount', 'RetweetCount', 'LikeCount','Source', 'Url'])
@@ -35,51 +29,49 @@ if word:
 else:
     st.warning(option,' cant be empty', icon="⚠️")
 
+#SIDEBAR
+with st.sidebar:   
+    st.info('DETAILS', icon="ℹ️")
+    if option=='Keyword':
+        st.info('Keyword is '+word)
+    else:
+        st.info('Hashtag is '+word)
+    st.info('Starting Date is '+str(start))
+    st.info('End Date is '+str(end))
+    st.info("Number of Tweets "+str(tweet_c))
+    st.info("Total Tweets Scraped "+str(len(tweets_df)))
+    x=st.button('Show Tweets',key=1)
+
 # DOWNLOAD AS CSV
 @st.cache # IMPORTANT: Cache the conversion to prevent computation on every rerun
 def convert_df(df):    
     return df.to_csv().encode('utf-8')
 
 if not tweets_df.empty:
-    csv = convert_df(tweets_df)
-    st.download_button(label="Download data as CSV",data=csv,file_name='Twitter_data.csv',mime='text/csv',)
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        csv = convert_df(tweets_df) # CSV
+        c=st.download_button(label="Download data as CSV",data=csv,file_name='Twitter_data.csv',mime='text/csv',)        
+    with col2:    # JSON
+        json_string = tweets_df.to_json(orient ='records')
+        j=st.download_button(label="Download data as JSON",file_name="Twitter_data.json",mime="application/json",data=json_string,)
 
-    # DOWNLOAD AS JSON
-    json_string = tweets_df.to_json(orient ='records')
-    st.download_button(label="Download data as JSON",file_name="Twitter_data.json",mime="application/json",data=json_string,)
+    with col3: # SHOW
+        y=st.button('Show Tweets',key=2)
 
-    # UPLOAD DATA TO DATABASE
-    if st.button('Upload Tweets to Database'):
-        coll=word
-        coll=coll.replace(' ','_')+'_Tweets'
-        mycoll=mydb[coll]
-        dict=tweets_df.to_dict('records')
-        if dict:
-            mycoll.insert_many(dict) 
-            ts = time.time()
-            mycoll.update_many({}, {"$set": {"KeyWord_or_Hashtag": word+str(ts)}}, upsert=False, array_filters=None)
-            st.success('Successfully uploaded to database', icon="✅")
-            st.balloons()
-        else:
-            st.warning('Cant upload because there are no tweets', icon="⚠️")
+if c:
+    st.success("The Scraped Data is Downloaded as .CSV file:",icon="✅")  
+if j:
+    st.success("The Scraped Data is Downloaded as .JSON file",icon="✅")     
+if x: # DISPLAY
+    st.success("The Scraped Data is:",icon="✅")
+    st.write(tweets_df)
+if y: # DISPLAY
+    st.balloons()
+    st.success("Tweets Scraped Successfully:",icon="✅")
+    st.write(tweets_df)
 
-    # SHOW TWEETS
-    if st.button('Show Tweets'):
-        st.write(tweets_df)
-
-# SIDEBAR
-with st.sidebar:   
-    st.write('Uploaded Datasets: ')
-    for i in mydb.list_collection_names():
-        mycollection=mydb[i]
-        #st.write(i, mycollection.count_documents({}))        
-        if st.button(i):            
-            dfm = pd.DataFrame(list(mycollection.find())) 
-
-# DISPLAY THE DOCUMENTS IN THE SELECTED COLLECTION
-if not dfm.empty: 
-    st.write( len(dfm),'Records Found')
-    st.write(dfm) 
+    
 
 
             
